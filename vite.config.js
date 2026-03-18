03896a4819c402262a95700610a7ccc6d9593615
Nur Alam <nuralam1122345@gmail.com>
2026-03-18 17:32:55 +0600

    first commit

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // এই লাইনটি নিশ্চিত করুন
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(), // এখানে প্লাগিনটি কল করা হয়েছে
+  ],
+})
